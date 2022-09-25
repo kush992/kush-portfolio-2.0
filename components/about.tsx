060c7b96050a7ffec1ee94c703d5863918074405
Kush <85061303+kush992@users.noreply.github.com>
2022-09-25 16:41:24 +0200

    responsive component: about

--- components/about.tsx
+++ components/about.tsx
@@ -17,11 +17,11 @@ const About = ({ pageInfo}: Props) => {
                 transition={{ duration: 1.2 }}
                 alt='kush-bhalodi-img'
                 src={urlFor(pageInfo?.profilePic).url()}
-                className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+                className='-mb-20 md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
             />
             <div className='space-y-10 px-0 md:px-10'>
-                <h4 className='text-4xl font-semibold'>Here is a <span className='underline decoration-[#f7ab0a]'>little</span> background</h4>
-                <p className='text-base'>{pageInfo?.backgroundInformation}</p>
+                <h4 className='text-xl md:text-4xl font-semibold'>Here is a <span className='underline decoration-[#f7ab0a]'>little</span> background</h4>
+                <p className='text-sm md:text-base'>{pageInfo?.backgroundInformation}</p>
             </div>
         </div>
     )
