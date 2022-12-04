801b12bb4bce4f1aecc4120c8c3d73acec4ff3b6
Kush <85061303+kush992@users.noreply.github.com>
2022-12-04 21:39:23 +0100

    removed skill progress circle - temporary

--- components/skillCircle.tsx
+++ components/skillCircle.tsx
@@ -17,15 +17,6 @@ const SkillCircle = ({ skill }: Props) => {
         src={urlFor(skill?.image).url()}
         className={`rounded-full border border-gray-500 object-cover w-14 h-14 xl:w-20 xl:h-20 filter transition duration-300 ease-in-out ${skill?.progress && 'group-hover:grayscale'}`}
       />
-      {skill?.progress && (
-        <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-14 h-14 xl:w-20 xl:h-20 rounded-full z-0">
-          <div className="flex items-center justify-center h-full">
-            <p className="text-1xl font-bold text-black opacity-100">
-              {skill?.progress}%
-            </p>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
